3aa4c4c587b1293abce8d0ae3cd0da17898e04e3
CyberTechStream <cybertechstream@gmail.com>
2025-11-22 10:02:22 +0200

    Update game.py

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import random
 
-
+# --- КОНСТАНТИ ---
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 FPS = 60
@@ -15,11 +15,12 @@ SKY_BLUE = (135, 206, 235)
 GREEN = (34, 139, 34)
 DARK_GREEN = (0, 100, 0)
 
+# Налаштування
 GAP_SIZE = 150
 GRAVITY = 0.25
 JUMP_STRENGTH = -6
 
-
+# --- КЛАСИ ---
 class Bird:
     def __init__(self):
         self.width = 40
@@ -28,8 +29,10 @@ class Bird:
         self.y = SCREEN_HEIGHT // 2
         self.velocity = 0
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        # дописати картинки
 
-        # ДОПИСАТИ завантаження картинок
+
+
 
     def jump(self):
         ...
@@ -38,7 +41,6 @@ class Bird:
         ...
 
     def draw(self, screen):
-        # розрахунок центрування
         center_x = self.rect.x + (self.rect.width // 2)
         center_y = self.rect.y + (self.rect.height // 2)
 
@@ -58,13 +60,12 @@ class Pipe:
     def __init__(self):
         self.width = 60
         self.x = SCREEN_WIDTH
+        # відст. між трубами
         self.gap_y = random.randint(100, SCREEN_HEIGHT - 100 - GAP_SIZE)
         self.speed = 3
         self.passed = False
-
-        # верхня труба
+        # врех. та нижн. труби
         self.top_rect = pygame.Rect(self.x, 0, self.width, self.gap_y)
-        # нижня труба
         self.bottom_rect = pygame.Rect(self.x, self.gap_y + GAP_SIZE, self.width,
                                        SCREEN_HEIGHT - (self.gap_y + GAP_SIZE))
 
@@ -72,21 +73,18 @@ class Pipe:
         ...
 
     def draw(self, screen):
-       ...
+        ...
 
 
 # --- ГОЛОВНИЙ КЛАС ---
-
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption("Flappy Bird")
-        self.clock = pygame.time.Clock()
-        self.font = pygame.font.SysFont('Arial', 24)
-        self.big_font = pygame.font.SysFont('Arial', 40)
+        # дописати
 
-        # Дописати налаштування
+
 
     def reset_round(self):
         ...
@@ -101,39 +99,31 @@ class Game:
                     self.running = False
 
                 if event.type == pygame.KEYDOWN:
-                    # Логіка запуску гри
-                    if event.key == pygame.K_SPACE:
-                        # Дописати стрибок
+                    ...
+                    # дописати
 
-                    # Дописати логіку перезапуску після Game Over
-
-
-            # --- ЛОГІКА ГРИ (Працює тільки якщо гра почалась і не закінчилась) ---
             if self.game_started and not self.game_over:
-                # Рух пташки, труб
-                ...
+                 # дописати
 
             elif not self.game_started:
-                # --- ЕКРАН ОЧІКУВАННЯ ---
-                self.bird.draw(self.screen)  # Малюємо пташку, яка стоїть на місці
+                self.bird.draw(self.screen)
 
                 start_text = self.big_font.render("Press SPACE", True, BLACK)
                 start_text2 = self.big_font.render("to Start", True, BLACK)
+
                 self.screen.blit(start_text, (SCREEN_WIDTH // 2 - 120, SCREEN_HEIGHT // 2 - 50))
                 self.screen.blit(start_text2, (SCREEN_WIDTH // 2 - 80, SCREEN_HEIGHT // 2 + 10))
 
             else:
-                # --- ЕКРАН СМЕРТІ ---
                 txt_go = self.big_font.render("GAME OVER", True, BLACK)
                 txt_r = self.font.render("Press 'R' to Restart", True, BLACK)
                 self.screen.blit(txt_go, (SCREEN_WIDTH // 2 - 120, SCREEN_HEIGHT // 2 - 50))
                 self.screen.blit(txt_r, (SCREEN_WIDTH // 2 - 110, SCREEN_HEIGHT // 2 + 10))
 
-            # ДОПИСАТИ Інтерфейс
-
-
-
-            
+            score_surf = self.font.render(f"Score: {self.score}", True, WHITE)
+            score_shadow = self.font.render(f"Score: {self.score}", True, BLACK)
+            self.screen.blit(score_shadow, (12, 12))
+            self.screen.blit(score_surf, (10, 10))
 
             pygame.display.flip()
 
